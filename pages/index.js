d3dfc2c0a1cd0176daff88bc46b01e063449392c
banant <banant@umich.edu>
2023-08-13 16:58:14 -0400

    mobile

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <title>Anant Bajaj</title>
         <link rel="icon" href="/images/A.png" />
       </Head>
